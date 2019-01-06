10de99d29c086329e71ab0e9f3fc1bec2be2a17f
Gao With MacBook Pro <rmanfour@gmail.com>
2019-01-07 00:16:33 +0800

    koa-router

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const Koa = require('koa');
 
+const router = require('koa-router')();
+
 const app = new Koa();
 
 app.use(async (ctx, next) => {
-  console.log(`${ctx.request.method}`, `${ctx.request.url}`);
+  console.log(`Process ${ctx.request.method}, ${ctx.request.url}...`);
   await next();
 });
 
-app.use(async (ctx, next)=>{
-    const start = new Date().getTime();
+// add url-router
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>hello ${name}</h1>`
     await next();
-    const ms = new Date().getTime() - start;
-    console.log(`Time: ${ms}ms`)
 })
-app.use(async (ctx, next) => {
-  await next();
-  ctx.response.type = 'text/html';
-  ctx.response.body = '<h1>Hello</h1>';
-});
+
+router.get('/', async (ctx, next)=>{
+    ctx.response.body = '<h1>Index</h1>'
+})
+
+app.use(router.routes());
+// app.use(async (ctx, next)=>{
+//     const start = new Date().getTime();
+//     await next();
+//     const ms = new Date().getTime() - start;
+//     console.log(`Time: ${ms}ms`)
+// })
+// app.use(async (ctx, next) => {
+//   await next();
+//   ctx.response.type = 'text/html';
+//   ctx.response.body = '<h1>Hello</h1>';
+// });
 
 app.listen(3200);
 console.log('打开 http://localhost:3200/');
