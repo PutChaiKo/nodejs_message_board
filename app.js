5f46b820516f89355b5c8cfaf216cbc037e145e3
Gao With MacBook Pro <rmanfour@gmail.com>
2019-01-06 10:21:06 +0800

    添加了package.json

---
+++ app.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+app.listen(3200);
+console.log('app started at port 3000...');
+
+
+// const Koa = require('koa');
+// const app = new Koa();
+// app.use(async (ctx, next) => {
+//     await next();
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello, koa2!</h1>';
+// });
+
+// app.listen(3200);
+// console.log('app started at port 3000...');
