a8ef3a9750ec06227dc9ade1e3f03217fe4ca112
gao@HP <rmanfour@gmail.com>
2019-01-10 19:27:49 +0800

    勘误；

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+var fn_index = async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+    <form action="/sigin" method="post">
+    <p>Name: <input name="name" value="koa" /></p>
+    <p>password: <input name="password" type="password" /></p>
+    <p><input type="submit" value="Submit" /></p>
+    </form>
+    `;
+}
+
+var fn_signin = async (ctx, next) => {
+    var name = ctx.request.body.name || '';
+    var password = ctx.request.body.password || '';
+    console.log(`name: ${name}, pw: ${password}`);
+    if (name === 'koa' && password === '123456') {
+      ctx.response.body = `<h1>welcome ${name}</h1>`;
+    } else {
+      ctx.response.body = `<h1>Try again</h1>`;
+    }
+}
